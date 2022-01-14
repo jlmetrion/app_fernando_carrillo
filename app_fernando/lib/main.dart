a2a492148571320a4af97d8c50545fcdc42e57ec
jlmetrion <84740844+jlmetrion@users.noreply.github.com>
2022-01-14 00:39:32 -0400

    Entrar

--- app_fernando/lib/main.dart
+++ app_fernando/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:app_fernando/screens/home_screen.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,9 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-       home: HomeScreen()
-        );
-      
+        debugShowCheckedModeBanner: false, home: Center(child: HomeScreen()));
   }
 }
--- app_fernando/lib/screens/home_screen.dart
+++ app_fernando/lib/screens/home_screen.dart
@@ -5,8 +5,23 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Hola TU'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Creando Barra'),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(children: const <Widget>[
+          Text('Contador'),
+          Text('1'),
+          Expanded(
+            child: FittedBox(
+              fit: BoxFit.contain, // otherwise the logo will be tiny
+              child: FlutterLogo(),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
